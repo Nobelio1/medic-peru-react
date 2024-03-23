19c5fac85108cae07afe5c3bea841f2dd856d03e
TheTigerGoty <jairo.b0401@gmail.com>
2024-03-23 10:00:21 -0500

    [START] Base Proyecto

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const LandingPage:React.FC = () => {
+    return (
+        <div>
+            <h1>LandingPage</h1>
+        </div>
+    )
+}   
+
+export default LandingPage;
