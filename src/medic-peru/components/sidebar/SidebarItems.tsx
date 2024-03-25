1970352ce2ec75a48cc05a70c31807a35a9f5aec
TheTigerGoty <jairo.b0401@gmail.com>
2024-03-25 16:50:31 -0500

    [UPDATE] Sidebar Active

--- src/medic-peru/components/sidebar/SidebarItems.tsx
+++ src/medic-peru/components/sidebar/SidebarItems.tsx
@@ -1,34 +1,37 @@
 import { BarChart3, Boxes, LayoutDashboard, LifeBuoy, Package, Receipt, Settings, UserCircle } from "lucide-react";
 import { SidebarItem } from "./Sidebar";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 //!-------------------------------------------------------------------------------------------------------
 
 export default function SidebarItems() {
 
+    const location = useLocation();
+
     //!-------------------------------------------------------------------------------------------------------
 
     return (
         <main className="App">
-            <Link to={"/dashboard"}>
+            <NavLink to={"/dashboard"}>
                 <SidebarItem
                     icon={<LayoutDashboard size={30} />}
                     text="Dashboard"
                     alert
+                    active={location.pathname === "/dashboard"}
                 />
-            </Link>
+            </NavLink>
 
-            <Link to={"/profile"}>
-                <SidebarItem icon={<BarChart3 size={30} />} text="Pacientes" active />
-            </Link>
+            <NavLink to={"/profile"}>
+                <SidebarItem icon={<BarChart3 size={30} />} text="Pacientes" active={location.pathname === "/profile"} />
+            </NavLink>
 
-            <Link to={"/chat"}>
-                <SidebarItem icon={<UserCircle size={30} />} text="Chats" />
-            </Link>
+            <NavLink to={"/chat"}>
+                <SidebarItem icon={<UserCircle size={30} />} text="Chats" active={location.pathname === "/chat"} />
+            </NavLink>
 
-            <Link to={"/chat-ia"}>
-                <SidebarItem icon={<Boxes size={30} />} text="Chat IA" />
-            </Link>
+            <NavLink to={"/chat-ia"}>
+                <SidebarItem icon={<Boxes size={30} />} text="ChatIA" active={location.pathname === "/chat-ia"} />
+            </NavLink>
             {/* <SidebarItem icon={<Package size={30} />} text="Ordenes" alert />
             <SidebarItem icon={<Receipt size={30} />} text="Pagos" /> */}
             <hr className="my-3 border border-gray-400 " />
