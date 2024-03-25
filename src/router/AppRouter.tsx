74c0af7abb7a84e0458083e489cfed0800f6bd01
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-03-25 13:24:56 -0500

    [CAMBIO] - CREACION DEL APPROUTER

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom'
+import { RegisterDoctor, RegisterPacient } from '../auth'
+import { LandingPage, AuthPage, Dashboard, ChatPage, ChatIAPage, ProfilePage } from '../medic-peru'
+import WithAuth from '../auth/layout/WithAuth'
+import WithSidebar from '../medic-peru/layout/WithSidebar'
+
+export const AppRouter = () => {
+  return (
+    <> 
+    <Routes>
+        <Route path='/' element={<LandingPage />} />
+
+        <Route element={<WithAuth />}>
+          <Route path='/auth' element={<AuthPage />} />
+          <Route path='/register-doctor' element={<RegisterDoctor />} />
+          <Route path='/register-paciente' element={<RegisterPacient />} />
+        </Route>
+
+        <Route element={<WithSidebar />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/chat' element={<ChatPage />} />
+          <Route path='/chat-ia' element={<ChatIAPage />} />
+          <Route path='/register-doctor' element={<ProfilePage />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
